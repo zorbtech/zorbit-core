b0f59a27a42f53fe9d8917a271bd38b7d38fff34
Euan <euan_b_@hotmail.com>
2018-04-03 21:57:19 +0100

    add getdifficulty rpc command.
    add stub for getblockchaininfo

--- src/NBitcoin/RPC/RPCClient.cs
+++ src/NBitcoin/RPC/RPCClient.cs
@@ -36,7 +36,7 @@ namespace NBitcoin.RPC
         network            clearbanned
 
         ------------------ Block chain and UTXO
-        blockchain         getblockchaininfo
+        blockchain         getblockchaininfo            TODO
         blockchain         getbestblockhash             Yes
         blockchain         getblockcount                Yes
         blockchain         getblock                     Yes
@@ -52,11 +52,11 @@ namespace NBitcoin.RPC
         blockchain         verifychain
 
         ------------------ Mining
-        mining             getblocktemplate
-        mining             getmininginfo
-        mining             getnetworkhashps
-        mining             prioritisetransaction
-        mining             submitblock
+        mining             getblocktemplate             TODO
+        mining             getmininginfo                TODO
+        mining             getnetworkhashps             TODO
+        mining             prioritisetransaction        TODO
+        mining             submitblock                  TODO
 
         ------------------ Coin generation
         generating         getgenerate
@@ -930,6 +930,11 @@ namespace NBitcoin.RPC
 #endregion
 
 #region Block chain and UTXO
+
+        public double GetDifficulty()
+        {
+            return (double)SendCommand(RPCOperations.getdifficulty).Result;
+        }
 
         public uint256 GetBestBlockHash()
         {
--- src/Stratis.Bitcoin.Features.Consensus/ConsensusController.cs
+++ src/Stratis.Bitcoin.Features.Consensus/ConsensusController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
+using NBitcoin.RPC;
 using Stratis.Bitcoin.Base;
+using Stratis.Bitcoin.Features.BlockStore;
 using Stratis.Bitcoin.Features.Consensus.Interfaces;
 using Stratis.Bitcoin.Utilities;
 
@@ -19,6 +21,30 @@ namespace Stratis.Bitcoin.Features.Consensus
         {
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
             this.ConsensusLoop = consensusLoop;
+        }
+
+        [ActionName("getblockchaininfo")]
+        [ActionDescription("Get ")]
+        public BlockChainInfo GetBlockChainInfo()
+        {
+            var chainInfo = new BlockChainInfo();
+            chainInfo.Chain = this.Chain.Network.ToString();
+            chainInfo.Blocks = this.ChainState.ConsensusTip.Height;
+            // chainInfo.Headers = this.ChainState.ConsensusTip.ToString();
+            chainInfo.BestBlockHash = this.ChainState.ConsensusTip.HashBlock.ToString();
+            chainInfo.Difficulty = this.GetNetworkDifficulty();
+            chainInfo.MedianTime = this.Chain.Tip.GetMedianTimePast().ToUnixTimeSeconds();
+            chainInfo.VerificationProgress = this.GetVerificationProgress();
+            chainInfo.Pruned = this.GetPruneStatus();
+
+            return chainInfo;
+        }
+
+        [ActionName("getdifficulty")]
+        [ActionDescription("Get the current proof of work difficulty")]
+        public double GetDifficulty()
+        {
+            return this.GetNetworkDifficulty();
         }
 
         [ActionName("getbestblockhash")]
@@ -44,6 +70,29 @@ namespace Stratis.Bitcoin.Features.Consensus
                 return null;
             ChainedBlock block = this.Chain.GetBlock(height);
             return block == null || block.Height > bestBlock.Height ? null : block.HashBlock;
+        }
+
+        private double GetVerificationProgress()
+        {
+            if (this.Chain.Tip == null)
+                return 0.0;
+
+            // requires total transaction count,
+            // timestamp of last known number of transactions,
+            // estimated number of transactions per second since timestamp
+
+            return 1.0;
+        }
+
+        private bool GetPruneStatus()
+        {
+            StoreSettings blockSettings = (StoreSettings)this.FullNode.Services.ServiceProvider.GetService(typeof(StoreSettings));
+            return blockSettings.Prune;
+        }
+
+        private double GetNetworkDifficulty()
+        {
+            return this.ConsensusLoop.Tip.GetWorkRequired(this.Chain.Network)?.Difficulty ?? 0.0;
         }
     }
 }
